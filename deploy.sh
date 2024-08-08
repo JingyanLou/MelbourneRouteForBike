a5756c9006571ddb4f2d6f1656efe196f7181fb6
Jingyan LOU <jlou0008@student.monash.edu>
2024-08-08 16:34:56 +1000

    CD pipeline test v4

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 # Print debug information
 echo "Docker username: $DOCKER_USERNAME"
 
-
 # Navigate to the home directory of the ubuntu user
 cd /home/ubuntu
 
@@ -11,11 +10,17 @@ cd /home/ubuntu
 docker pull $DOCKER_USERNAME/my-app-frontend:latest
 docker pull $DOCKER_USERNAME/my-app-backend:latest
 
-# Stop and remove the existing containers if they are running
-docker stop frontend || true
-docker rm frontend || true
-docker stop backend || true
-docker rm backend || true
+# Stop and remove the existing frontend container if it is running
+if [ "$(docker ps -q -f name=frontend)" ]; then
+    docker stop frontend
+    docker rm frontend
+fi
+
+# Stop and remove the existing backend container if it is running
+if [ "$(docker ps -q -f name=backend)" ]; then
+    docker stop backend
+    docker rm backend
+fi
 
 # Run the frontend container
 docker run -d --name frontend -p 80:3000 $DOCKER_USERNAME/my-app-frontend:latest
