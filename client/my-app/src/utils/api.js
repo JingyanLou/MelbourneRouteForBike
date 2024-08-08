de46c610d8745237cc11ba444fe1a03e3fe4d17e
Jingyan LOU <jlou0008@student.monash.edu>
2024-08-08 17:21:49 +1000

    Add logging to api.js to debug environment variables

--- client/my-app/src/utils/api.js
+++ client/my-app/src/utils/api.js
@@ -1,5 +1,9 @@
 // src/utils/api.js
 export const getApiBaseUrl = () => {
+    console.log('NODE_ENV:', process.env.NODE_ENV);
+    console.log('REACT_APP_API_URL_LOCAL:', process.env.REACT_APP_API_URL_LOCAL);
+    console.log('REACT_APP_API_URL_PROD:', process.env.REACT_APP_API_URL_PROD);
+
     return process.env.NODE_ENV === 'development'
         ? process.env.REACT_APP_API_URL_LOCAL
         : process.env.REACT_APP_API_URL_PROD;
