0a362a15ab1252d066afaa77db70d29ac77ec9b7
Jingyan LOU <jlou0008@student.monash.edu>
2024-08-08 16:20:21 +1000

    CD pipeline testing v2

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-cd /home/ec2-user
+
+# Navigate to the home directory of the ubuntu user
+cd /home/ubuntu
 
 # Pull the latest Docker images
 docker pull $DOCKER_USERNAME/my-app-frontend:latest
 docker pull $DOCKER_USERNAME/my-app-backend:latest
 
-# Stop and remove existing containers
+# Stop and remove the existing containers if they are running
 docker stop frontend || true
 docker rm frontend || true
 docker stop backend || true
@@ -16,5 +18,3 @@ docker run -d --name frontend -p 80:3000 $DOCKER_USERNAME/my-app-frontend:latest
 
 # Run the backend container
 docker run -d --name backend -p 5000:5000 $DOCKER_USERNAME/my-app-backend:latest
-
-
